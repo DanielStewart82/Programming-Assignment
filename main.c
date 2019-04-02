170bd02c1f5ab061e8b820c5ccfea7b09592f8e2
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-02 00:55:35 +0000

    Testing the save feature before I go far and Che crashes and then I go nuts.

--- main.c
+++ main.c
@@ -4,5 +4,10 @@
 
 int main()
 {
+    //need to create a string to hold the message to be encrypted.  Around 200 characters.
+    char encryptedString[200];
+    
+    printf("Enter text to be encrypted:\n");
+    scanf("%s", encryptedString);
    printf("This is the first line of code for my programming assignment");
 }
