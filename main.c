e4f955ba491650f77520ddc9aa571b74c00ab989
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-09 01:35:04 +0000

    Working code.

--- main.c
+++ main.c
@@ -21,17 +21,17 @@ void enigmaFunction(char *regularText, int n)
     {
     int i;
     int key = 0;
-    for( i=0; i <= n ; i++ )
+    for( i=0; i <= (n-2) ; i++ )
     {    if(97<=regularText[i] && regularText[i]<=122){
         regularText[i]= regularText[i]-32;
-       
     }
-      
+     
     regularText[i]= regularText[i]-65;
     regularText[i]=(regularText[i]+key)%(26);
     regularText[i]=regularText[i]+65;
-    printf("%c", regularText[i] );
-      
+    }     
+
     
-   // printf("\n" );
-}}
+  
+   printf("%s\n", regularText);
+}
