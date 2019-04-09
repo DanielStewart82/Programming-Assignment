342281a293125f2027f35b8793a3531b5d9f6f99
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-09 02:25:32 +0000

    Please work

--- main.c
+++ main.c
@@ -20,22 +20,26 @@ int main()
 void enigmaFunction(char *regularText, int n)
     {
     int i;
-    int key = 0;
-    for( i=0; i <= (n-2) ; i++ )
-    {    if(97<=regularText[i] && regularText[i]<=122){
-        regularText[i]= regularText[i]-32;
+    int key = -1;
+    
+        for( i=0; i <= (n-2) ; i++ )
+    {        if(97<=regularText[i] && regularText[i]<=122){
+             regularText[i]= regularText[i]-32;
     }
      
-    regularText[i]= regularText[i]-65;
+                regularText[i]= regularText[i]-65;
+                regularText[i]=(regularText[i]+key);
     
-    //for( i=0; i <= (n-2) ; i++ ){
-    if(regularText[i]<=0)
-    {regularText[i]=regularText[i]+26;}
-    regularText[i]=(regularText[i]+key)%(26);
-    regularText[i]=regularText[i]+65;
-    }     
-}
+                     if(regularText[i]<0)
+                     {
+                     regularText[i]=regularText[i]+25;
+                     }    
     
+                        regularText[i]=regularText[i]%(26);
+                        regularText[i]=regularText[i]+65;
+                         }     
+
+
   
-   printf("%s\n", regularText);
+                            printf("%s\n", regularText);
 }
