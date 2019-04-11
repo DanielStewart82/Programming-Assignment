1db1ec079e3cdc7341136a8d35fbb3b817d1947d
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-11 05:30:59 +0000

    This code is working and has a decryption function.

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 void enigmaFunction(char *regularText, int n);
+void decryptionFunction(char *decryptedText, int o);
 
 int main()
     {
     
-    char regularText[]= "attack at sunrise";
+    char regularText[]= "Hello Ben, how are you today?";
     
     int n;
     
@@ -20,24 +21,51 @@ int main()
 void enigmaFunction(char *regularText, int n)
     {
     int i;
-    int key = 52;
+    int key = 0;
     
         for( i=0; i <= (n-2) ; i++ )
         {
-            if(97<=regularText[i] && regularText[i]<=122){
+            if(97<=regularText[i] && regularText[i]<=122)
+            {
             regularText[i] = regularText[i]-32;
-        }
+            }
             if(65<=regularText[i] && regularText[i]<=90)
             {                
                 regularText[i]= regularText[i]-65;
                 regularText[i]=(regularText[i]+key);
-    
                 regularText[i]=regularText[i]%(26);
                 regularText[i]=regularText[i]+65;
             }     
 
-    }
+        }
 
-  
+    }
+    
+void decryptionFunction(char *decryptedText, int o)
+    {
+    int i;
+    int key = 23;
+    
+        for( i=0; i <= (o-2) ; i++ )
+        {
+            if(97<=decryptedText[i] && decryptedText[i]<=122){
+            decryptedText[i] = decryptedText[i]-32;
+        }
+            if(65<=decryptedText[i] && decryptedText[i]<=90)
+            {                
+                decryptedText[i]= decryptedText[i]-65;
+                decryptedText[i]=(decryptedText[i]-key);
+    
+                decryptedText[i]=decryptedText[i]%(26);
+                if(decryptedText[i]<0)
+                {
+                     decryptedText[i]=decryptedText[i]+26;
+                }
+                decryptedText[i]=decryptedText[i]+65;
+            }     
+            
+       
+            }
+          
 
 }
