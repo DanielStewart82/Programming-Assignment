0a533986ee089fcad004af0d5e3f37f15d99e6ce
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-11 04:32:02 +0000

    Working code.  This version will only roll forward with a positive key.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ void enigmaFunction(char *regularText, int n);
 int main()
     {
     
-    char regularText[]= "ABCDEFGHIJKLMN  poi tui O 235 PQRSTUVWXYZ abcdefghijklmnopqr 65 stuvwxyz";
+    char regularText[]= "attack at sunrise";
     
     int n;
     
@@ -19,27 +19,22 @@ int main()
  
 void enigmaFunction(char *regularText, int n)
     {
-    
     int i;
-    int key = -15;
+    int key = 52;
     
         for( i=0; i <= (n-2) ; i++ )
-    {        if(97<=regularText[i] && regularText[i]<=122){
-             regularText[i]= regularText[i]-32;
-    }
-            if(65<=regularText[i] && regularText[i]<=90){
-                
+        {
+            if(97<=regularText[i] && regularText[i]<=122){
+            regularText[i] = regularText[i]-32;
+        }
+            if(65<=regularText[i] && regularText[i]<=90)
+            {                
                 regularText[i]= regularText[i]-65;
                 regularText[i]=(regularText[i]+key);
     
-                     if(regularText[i]<=0)
-                     {
-                     regularText[i]=regularText[i]+26;
-                     }    
-    
-                        regularText[i]=regularText[i]%(26);
-                        regularText[i]=regularText[i]+65;
-                         }     
+                regularText[i]=regularText[i]%(26);
+                regularText[i]=regularText[i]+65;
+            }     
 
     }
 
