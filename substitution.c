112cf64f502525154cd0b370f9dee66a75decaa9
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-20 06:45:11 +0000

    THis substitution code works

--- substitution.c
+++ substitution.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 
-void ekey(char subString[], char encryptedString[], int n);
+void ekey(char *messageString, char *substitutionString);
+
 
 int main(){
     
+char substitutionString[]="QWERTYUIOPASDFGHJKLZXCVBNM";
 
-    char subString[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
-    int n;
-    n = sizeof(subString)/(sizeof(subString[0]));
-    char encryptedString[1000];
-    ekey(subString, encryptedString, n);
-    printf("%s\n", encryptedString);
+char messageString[]= "PLEASE GET MILK AT THE SHOPS";
+    //int x;
+   // x = (sizeof(messageString))/(sizeof(messageString[0]));    
 
-   //printf("%c\n", subString[22]);
-   
+    ekey(messageString, substitutionString);    
+    printf("\n");
+    //printf("%s\n", messageString);
     return 0;
 }
-void ekey(char subString[], char encryptedString[], int n){
-    int i;
-    for(i=0; i <= n-2; i++){
-        subString[i]=subString[i]-65;
-        encryptedString[i]=subString[i];
-        
+
+
+void ekey(char *messageString, char *substitutionString)
+{
+    int i, n;
+    for(i=0; messageString[i]!= '\0' ; i++)
+    {
+        if(messageString[i]<= 90 && messageString[i]>=65)
+        {            
+             n=messageString[i]-65; 
+         //messageString[i]=messageString[substitutionString[n]];
+           printf("%c", substitutionString[n]);
+        }
+        else
+        {
+            printf("%c", messageString[i]);
+
+        }
+    
+    
     }
 }
