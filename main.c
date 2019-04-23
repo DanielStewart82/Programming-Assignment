5790bc6823452f53c7d5130b6b8308bd24af001d
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-23 00:42:48 +0000

    Starting to ad comments.

--- main.c
+++ main.c
@@ -1,31 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*Welcome to the ENGG1003 Programming Assignment One for Semester 1 of 2019. 
+This guide and the following notes are intended to be read and understood by a student 
+who is repeating the course.*/
 
-void enigmaFunction(char *regularText, int n, int encryptKey);
-void decryptionFunction(char *decryptedText, int o, int decryptKey);
-void substitutionEncryption(char *messageString, char *substitutionString);
-void decrypt(char *encryptedMessage, char *substitutionString);
+
+#include <stdio.h>
+
+/*This program utilises four functions to operate.
+Functions must be declared at the beginning of the program.
+The function definitions are placed at the end of the program.  
+Directly underneath this comment are the four function declarations.*/
+
+void enigmaFunction(char *regularText, int n, int encryptKey);  //Encryption with rotation cipher.
+void decryptionFunction(char *decryptedText, int o, int decryptKey); //Decryption with rotation cipher.
+void substitutionEncryption(char *messageString, char *substitutionString);//Encryption with substitution cipher.
+void decrypt(char *encryptedMessage, char *substitutionString); //Decryption with substitution cipher.
+
 int main()
     {
-        int encryptKey;
-        int decryptKey;
-        int task;
+        int encryptKey;  //users will be prompted to input a number between 0 & 25 to rotate the alphabet.
+        int decryptKey;  //users will be prompted to input a number between 0 & 25 to un-rotate the alphabet.
+        int task;        //used for task selection in the menu.  User will input this value.
         printf("*****************************************************************************************************************\n");
-        printf("Welcome to the ENGG1003 Programming Assignment. 1\n");
+        printf("Welcome to the ENGG1003 Programming Assignment.\n");
         printf("This program was written by Daniel Stewart.  Student Number: c3298707\n");
+        printf("All work is the work of myself and the program was completed on 28th April 2019.\n");
+        printf("\n");
+        printf("\n");
         printf("This program will decrypt or encrypt a message using either a rotation cipher or a substitution cipher.\n");
         printf("All inputs will need to be hardcoded prior to running this program.\n");
+        printf("\n");
         printf("Task selection:\n");
+        printf("\n");
         printf("Task 1: Encryption with rotation cipher\n");
         printf("Task 2: Decryption with rotation cipher\n");
         printf("Task 3: Encryption with substitution cipher\n");
         printf("Task 4: Decryption with substitution cipher\n");
+        printf("\n");
         printf("Please make your selection now by entering the task number you wish to select then press enter.\n");
         scanf("%d", &task);
        
+   /*  Between line 45 and line 57 is where the text that users wish to encrypt or decrypt
+   needs to be inputed.  
         
-        
-        
+        */
     char regularText[]= "MON MOTHMA: THE DATA BROUGHT TO US BY THE BOTHAN SPIES PINPOINTS THE EXACT LOCATION OF THE EMPEROR'S NEW BATTLE STATION. WE ALSO KNOW THAT THE WEAPON SYSTEMS OF THIS DEATH STAR ARE NOT YET OPERATIONAL. WITH THE IMPERIAL FLEET SPREAD THROUGHOUT THE GALAXY IN A VAIN EFFORT TO ENGAGE US, IT ISRELATIVELY UNPROTECTED. BUT MOST IMPORTANT OF ALL, WE'VE LEARNED THAT THE EMPEROR HIMSELF IS PERSONALLY OVERSEEING THE FINAL STAGES OF THE CONSTRUCTION OF THIS DEATH STAR. MANY BOTHANS DIED TO BRING US THIS INFORMATION.";
     int n;
     n = (sizeof(regularText))/sizeof(regularText[0]);
@@ -48,16 +65,21 @@ int main()
           case 1:
               printf("*****************************************************************************************************************\n");
               printf("The hardcoded message you wish to encrypt with the rotation cipher is:\n");
+              printf("\n");       
               printf("%s\n", regularText);
+              printf("\n");
               printf("Enter an integer between 0 and 25 that you wish to rotate the alphabet by:\n");
               scanf("%d", &encryptKey);
-              printf("You have chosen to rotate the alphabet by a value of:%d\n", encryptKey);
+              printf("You have chosen to rotate the alphabet by a value of: %d\n", encryptKey);
+              printf("\n");
               printf("Your encrypted message is:\n");
+              printf("\n");
           
               enigmaFunction(regularText, n, encryptKey);
                  
                  
               printf("%s\n", regularText);
+              printf("\n");
               printf("End program.\n");
             
           break;
@@ -65,14 +87,21 @@ int main()
             case 2:
               printf("*****************************************************************************************************************\n");
               printf("The hardcoded message you wish to decrypt with the rotation cipher is:\n");
+              printf("\n");
               printf("%s\n", decryptedText);
+              printf("\n");
               printf("Enter an integer between 0 and 25 that you wish to rotate the alphabet by:\n");
+              printf("\n");
               scanf("%d", &decryptKey);
+              printf("\n");
               printf("You have chosen to rotate the alphabet by a value of:%d\n", decryptKey);
+              printf("\n");
               printf("Your decrypted message is:\n");
+              printf("\n");
           
               decryptionFunction(decryptedText, o, decryptKey);
               printf("%s\n", decryptedText);
+              printf("\n");
                  
                  
               printf("End program.\n");
@@ -82,12 +111,17 @@ int main()
           case 3:
               printf("*****************************************************************************************************************\n");     
               printf("The hardcoded message you wish to encrypt with the substitution cipher is:\n");
+              printf("\n");
               printf("%s\n", messageString);
+              printf("\n");
               printf("The hardcoded substitution you have entered is:%s\n", substitutionString); 
+              printf("\n");
               printf("Your encrypted message is:\n");
+              printf("\n");
           
                                            
               substitutionEncryption(messageString, substitutionString);    
+              printf("\n");
               printf("\n");
               printf("End program.\n");
             
@@ -96,15 +130,24 @@ int main()
             case 4:
               printf("*****************************************************************************************************************\n");        
               printf("The hardcoded message you wish to decrypt with the substitution cipher is:\n");
+              printf("\n");
               printf("%s\n", encryptedMessage);
+              printf("\n");
               printf("The hardcoded substitution you have entered is:%s\n", substitutionString); 
-              printf("Your decrypted message is:\n");              
+              printf("\n");
+              printf("Your decrypted message is:\n");
+              printf("\n");
               
               decrypt(encryptedMessage, substitutionString);
+              printf("\n");
               printf("\n");
               printf("End program.\n");
             
           break;
+          
+              default:
+              printf("Invalid selection.  %d not recognised.\n", task);
+              break;
         }
     }
 
