bfb417cad6c0eaedc479b9ac68cd941fb5a9a6fe
Daniel Stewart <sailordan.coyote@gmail.com>
2019-05-06 05:17:04 +0000

    Keeps a sum.

--- PASS/2darray.c
+++ PASS/2darray.c
@@ -4,10 +4,13 @@ int main(){
     
     int a[3][4]={{7,1,2,3},{4,5,6,7},{8,9,10,11}};
     int b[3];
-    int i, j, min;
+    int i, j, min, sum;
     for(i=0;i<3;i++){
+        //sum = a[i][0];
+        
         min = a[i][0];
         for(j=0;j<4;j++){
+            sum = sum + a[i][j];
             
             printf("%d ", a[i][j]);  
 
@@ -22,7 +25,8 @@ int main(){
         }
         //printf("%d\n", min);
         b[i]=min;
-        printf("%d\n", b[i]);
-    }
-    
+       // printf("%d\n", b[i]);
+        printf("\n");
+    }        printf("%d\n", sum);
+    //printf("%d", b[3]);
 }
