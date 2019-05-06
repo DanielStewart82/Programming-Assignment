f635db94af121576a22371ba2e90a173a0bc04af
Daniel Stewart <sailordan.coyote@gmail.com>
2019-05-06 04:49:43 +0000

    Will return lowest number from each array.

---
+++ PASS/2darray.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a[3][4]={{7,1,2,3},{4,5,6,7},{8,9,10,11}};
+    int i, j, min;
+    for(i=0;i<3;i++){
+        min = a[i][0];
+        for(j=0;j<4;j++){
+            
+            printf("%d ", a[i][j]);  
+
+            
+            if(a[i][j]<min){
+                min=a[i][j];
+            }
+
+           
+            
+        }
+        printf("%d\n", min);
+    }
+    
+}
